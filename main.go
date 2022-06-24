af62160a86aed832d7b1d23d2bd9c7850eeb2f92
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-06-23 22:33:25 -0400

    add unmarshal example

--- main.go
+++ main.go
@@ -26,5 +26,13 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println("print JSON=", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("back into a Go data structure=", xp2)
 }
